dc2b41590fb713d3dde9d5607ddaf2ee96cb7ae2
DevJhojan <jhojan1806@gmail.com>
2023-10-03 09:36:55 -0500

    Se limpio la lista de pokemonst

--- pokedesk/src/app/Components/list-pomemon/list-pomemon.component.ts
+++ pokedesk/src/app/Components/list-pomemon/list-pomemon.component.ts
@@ -2,69 +2,53 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { debounce, debounceTime, map } from 'rxjs';
+import { IPokemonModel } from 'src/Models/pokemon.model';
 import { PokedeskService } from 'src/app/Service/pokedesk.service';
-const pokemon_count = 1281;
+const pokemon_count = 100;
 @Component({
   selector: 'app-list-pomemon',
   templateUrl: './list-pomemon.component.html',
   styleUrls: ['./list-pomemon.component.scss'],
 })
 export class ListPomemonComponent implements OnInit {
-  pokemons: any[] = [];
-  pokenShow: any[] = [];
+  pokemons: IPokemonModel[] = [];
+  pokenShow: IPokemonModel[] = [];
   name!: string;
   searchGroup!: FormGroup;
   constructor(private poken: PokedeskService, private router: Router) {
     this.getPokemons();
-
   }
 
   ngOnInit(): void {
-    this.searchGroup = new FormGroup({
-      searchInput: new FormControl('')
-    });
-    /* this.getPokemons(); */
 
-    this.searchPokemons();
   }
 
-  searchPokemons() {
-
-    try {
-      this.searchGroup.valueChanges
-      .pipe(debounceTime(1000))
-      .subscribe(querry=>{
-        this.pokemons = [];
-        if(querry.searchInput == ''){
-          this.getPokemons();
-        }else{
-          this.pokenShow = [];
-          this.poken.getSearchPokemon(querry.searchInput)
-          .subscribe((data:any)=>{
-            this.pokenShow.push(data);
-          });
-        }
-      })
-    } catch (error) {
-      console.log(error);
-    }
-    /* this.name = this.searchGroup.value;
-
-    this.poken.getSearchPokemon(this.name).subscribe(); */
-
-
+  searchPokemons(e: Event) {
+    setTimeout(()=>{
+      const filtro = (e.target as HTMLInputElement).value;
+      if(filtro.length === 0){
+        this.getPokemons();
+      }else{
+        let pokemons: IPokemonModel[] = this.pokenShow.filter((pokemon)=> pokemon.name.toLowerCase().includes(filtro.toLowerCase()));
+        this.pokenShow = []
+        this.pokenShow= pokemons;
+        console.log(this.pokenShow)
+      }
+  },500)
   }
 
   getPokemons() {
+    this.pokemons = [];
+    this.pokenShow = [];
     for (let i = 1; i <= pokemon_count; i++) {
-      this.poken.getPokemon(i).subscribe((data: any) => {
+      this.poken.getPokemon(i).subscribe((data: IPokemonModel) => {
         this.pokemons.push(data);
-        console.log(data);
       });
     }
     this.pokenShow = this.pokemons;
 
   }
+
   detailPokemon(index: number) {
     this.router.navigate([`/home/pokemonDetail/${index}`]);
   }
--- pokedesk/src/app/Service/pokedesk.service.ts
+++ pokedesk/src/app/Service/pokedesk.service.ts
@@ -1,16 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { IPokemonModel } from 'src/Models/pokemon.model';
 const basePokemon = 'https://pokeapi.co/api/v2/pokemon';
 @Injectable({
   providedIn: 'root'
 })
 export class PokedeskService {
- pokemons: any;
 
   constructor(private http: HttpClient) {}
 
-  getPokemon(id: number): Observable<any> {
+  getPokemon(id: number): Observable<IPokemonModel> {
     return this.http.get<any>(`${basePokemon}/${id}`).pipe();
   }
 
