b252b96dc3b973c1932783f43af8e5ff7c9d656c
DevJhojan <jhojan1806@gmail.com>
2023-10-12 11:27:59 -0500

    eliminación del concepto antiguo y creación del nuevo concepto

--- pokedesk-v2/README.md
+++ pokedesk-v2/README.md
@@ -1,6 +1,6 @@
-# Pokedesk
+# PokedeskV2
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.1.
 
 ## Development server
 
--- pokedesk-v2/src/app/app.component.spec.ts
+++ pokedesk-v2/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pokedesk'`, () => {
+  it(`should have as title 'pokedesk-v2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pokedesk');
+    expect(app.title).toEqual('pokedesk-v2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pokedesk app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pokedesk-v2 app is running!');
   });
 });
