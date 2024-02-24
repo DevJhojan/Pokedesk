38155b3aeb634a45a0bb12843c5efe1210353dfb
DevJhojan <jhojan1806@gmail.com>
2024-02-24 00:12:25 -0500

    Auto-generated commit

---
+++ assets/func/index.ts
@@ -0,0 +1,5 @@
+export { colorSheme } from './classes/colors-sheme';
+export { IColumns } from './interfaces/i-columns.interface';
+export { AbecedarioEnum } from "./enums/abecedario.enum";
+export { removeDuplicateData } from "./classes/remove-duplicate-data";
+
