790de5fa8d9e022f6827dc968fe82fbe1e328b84
DevJhojan <jhojan1806@gmail.com>
2024-04-22 17:46:04 -0500

    deleted some it

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,7 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
-import { MatTableModule } from '@angular/material/table';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { CdkTableModule } from '@angular/cdk/table';
 
@@ -16,7 +12,6 @@ import { SearchPokemonComponent } from './pages/search-pokemon/search-pokemon.co
 import { PokemonService } from '../Service';
 import { PokemonFrontComponent } from './pages/pokemon-front/pokemon-front.component';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { MaterialModule } from '../material/material.module';
 
 @NgModule({
