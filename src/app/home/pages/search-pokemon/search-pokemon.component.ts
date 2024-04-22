7f797400f083c2de59cd2f90c40b0f3cb0df2c81
DevJhojan <jhojan1806@gmail.com>
2024-04-22 18:49:51 -0500

    initializate pagination

--- src/app/home/pages/search-pokemon/search-pokemon.component.ts
+++ src/app/home/pages/search-pokemon/search-pokemon.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { IEntityModel } from '@models/*';
 import { PokemonService } from '@services/*';
@@ -10,21 +12,65 @@ import { PokemonService } from '@services/*';
   styles: [],
 })
 export class SearchPokemonComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  enablePaginator: boolean = true;
   pokemonsNames: IEntityModel[] = [];
   searchGroup: FormGroup;
   selector = '.main-panel';
   search = new FormControl<string>('');
-  limit: number = 100;
-  constructor(private pokemonService: PokemonService, private router: Router) {
+  limit: number = 10;
+  length = 10;
+  pageSize = 10;
+  pageIndex = 0;
+  pageSizeOptions = [5, 10, 25];
+  hidePageSize = true;
+  showPageSizeOptions = false;
+  showFirstLastButtons = true;
+  disabled = false;
+  pageEvent: PageEvent;
+  dataSource: any;
+  constructor(
+    private pokemonService: PokemonService,
+    private router: Router) {
+    this.pageEvent = new PageEvent();
     this.getPokemons();
     this.searchGroup = new FormGroup({
       searchInput: new FormControl<String>(''),
     });
   }
   ngOnInit(): void {}
+  ngAfterViewInit() {
+    this.paginator ? (this.dataSource.paginator = this.paginator) : '';
+  }
+  handlePageEvent(e: PageEvent) {
+    this.pageEvent = e;
+    this.length = e.length;
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
+    const startIndex = e.pageIndex * e.pageSize;
+    const endIndex = startIndex + e.pageSize;
+    const dataSlice = this.pokemonsNames.slice(startIndex, endIndex);
+    this.dataSource.data = dataSlice;
+  }
+  updatePaginator() {
+    this.length = this.dataSource.data.length;
+    this.paginator.length = this.length;
+    this.paginator.pageSize = this.pageSize;
+    this.paginator.pageIndex = this.pageIndex;
+    this.handlePageEvent(this.paginator);
+  }
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    if (setPageSizeOptionsInput) {
+      this.pageSizeOptions = setPageSizeOptionsInput
+        .split(',')
+        .map((str) => +str);
+    }
+  }
   getPokemons() {
     this.pokemonService.getAllPokemons(this.limit).subscribe((list) => {
       this.pokemonsNames = list.results;
+      this.dataSource = new MatTableDataSource(this.pokemonsNames);
+      this.updatePaginator();
     });
   }
   showMorePokemons(): void {
@@ -39,6 +85,7 @@ export class SearchPokemonComponent implements OnInit {
         );
         if (!exists) {
           this.pokemonsNames.push(pokemon);
+          this.updatePaginator()
         }
       });
     });
