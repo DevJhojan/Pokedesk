a0f63ea500e1903b2f1712d525dcbc2a7e9c35e3
DevJhojanXX <devjhojanxxii@gmail.com>
2024-09-14 19:45:55 -0500

    a little best 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   url: 'https://pokeapi.co/api/v2/pokemon',
-  urlAbilities: "https://pokeapi.co/api/v2/ability/",
-  urlEspecies: "https://pokeapi.co/api/v2/pokemon-species/",
-  allPokemon: '?limit=1000&offset=0',
+  urlAbilities: 'https://pokeapi.co/api/v2/ability/',
+  urlEspecies: 'https://pokeapi.co/api/v2/pokemon-species/',
+  allPokemon: '?limit=10000&offset=0',
 };
